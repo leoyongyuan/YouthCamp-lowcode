7d98d1397facead969c9262321b6939c0970899a
leoyongyuan <1359195795@qq.com>
2022-09-26 20:37:51 +0800

    create project

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import Vuetify from 'vuetify/lib'
+// To add vuetify css file
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
+  Vuetify,
   render: h => h(App)
 }).$mount('#app')
