71bfd6c2c65ba2edf28e78f28cac23c71c64cdc5
leoyongyuan <1359195795@qq.com>
2022-09-27 14:25:34 +0800

    update store

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    editMode: 'edit', // 编辑器模式 edit preview
+    canvasStyleData: { // 页面全局数据
+        width: 1200,
+        height: 740,
+        scale: 100,
+        color: '#000',
+        opacity: 1,
+        background: '#fff',
+        fontSize: 14,
+    },
+    isInEdiotr: false, // 是否在编辑器中，用于判断复制、粘贴组件时是否生效，如果在编辑器外，则无视这些操作
+    componentData: [], // 画布组件数据
+    curComponent: null,
+    curComponentIndex: null,
+    isClickComponent: false,
+    // 点击画布时是否点中组件，主要用于取消选中组件用。
+    // 如果没点中组件，并且在画布空白处弹起鼠标，则取消当前组件的选中状态
   },
   getters: {
   },
