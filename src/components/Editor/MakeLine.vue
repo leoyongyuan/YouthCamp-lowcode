060fe8c729ceb677d552469b785e14995806636b
leoyongyuan <1359195795@qq.com>
2022-10-21 22:55:29 +0800

    start build markline

---
+++ src/components/Editor/MakeLine.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="mark-line">
+
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+    data() {
+        return {
+            lines: ['xt', 'xc', 'xb', 'yl', 'yc', 'yr'],
+            diff: 3, // 相距 diff 像素将自动吸附
+            lineStatus: {
+                xt: false,
+                xc: false,
+                xb: false,
+                yl: false,
+                yc: false,
+                yr: false,
+            },
+        }
+    },
+    computed: mapState([
+        'curComponent',
+        'componentData',
+    ]),
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
